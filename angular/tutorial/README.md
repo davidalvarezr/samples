7d24550f2f003a481645c33891fab57e6015a453
David Alvarez <david.alvarezrestrepo@edu.hefr.ch>
2019-04-29 00:27:52 +0200

    Part 6 of tutorial just before Get hero by id

---
+++ angular/tutorial/README.md
@@ -0,0 +1,1 @@
+Based on https://angular.io/guide/quickstart#prerequisites
---
+++ angular/tutorial/my-app/src/app/hero.service.ts
@@ -0,0 +1,59 @@
+// Services are a great way to share information among classes that don't know each other
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  // That is how to do an asynchronous call
+  getHeroes(): Observable<Hero[]> {
+    this.log('HeroService: feteched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+ log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+ }
+}
